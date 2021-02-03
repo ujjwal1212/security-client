fb7fc64acc8e5d6f7cc65c771c256dd04064085b
Choudhary,Ujjwal <ujjwal.choudhary@honeywell.com>
2021-02-03 17:03:31 +0530

    first commit

---
+++ src/main/java/com/security/oauth/securityclient/SecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.security.oauth.securityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/oauth/securityclient/SecurityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.oauth.securityclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
